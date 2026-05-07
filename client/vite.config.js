585dcb3302824d1ebd87933db1b692fa43693ea8
Gaurav Kumar <kr.gauravbca7@gmail.com>
2026-05-07 14:31:42 +0530

    Backend URL Changed

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:6969',
+        target: 'https://xitamin-web-service-proposal.onrender.com',
         changeOrigin: true,
       },
     },
