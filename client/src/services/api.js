f9b7aae1c1118e4a7f97c59fddc5f3d2dceea19e
Gaurav Kumar <kr.gauravbca7@gmail.com>
2026-05-07 14:46:33 +0530

    backend url change

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_URL || '';
+const BASE_URL = 'https://xitamin-web-service-proposal.onrender.com';
 
 const api = axios.create({
   baseURL: BASE_URL,
