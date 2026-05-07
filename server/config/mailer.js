7b9193d402afa59745ccdd074a3d86c0a7e27a61
Gaurav Kumar <kr.gauravbca7@gmail.com>
2026-05-08 00:02:22 +0530

    hjhh

--- server/config/mailer.js
+++ server/config/mailer.js
@@ -5,7 +5,6 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
   port: Number(process.env.MAIL_PORT) || 587,
-  secure: Number(process.env.MAIL_PORT) ,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
